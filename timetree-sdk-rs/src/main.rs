ae6d80516a02a0f866ad9d9fd3dca1966fa24659
Keisuke Umeno <9renpoto@gmail.com>
2023-08-25 13:26:58 +0900

    chore: thinking about

--- timetree-sdk-rs/src/main.rs
+++ timetree-sdk-rs/src/main.rs
@@ -2,18 +2,10 @@ use std::error::Error;
 
 use reqwest_middleware::{ClientBuilder, ClientWithMiddleware, RequestBuilder};
 use reqwest_retry::{policies::ExponentialBackoff, RetryTransientMiddleware};
-use serde::{Deserialize, Serialize};
+use serde::Deserialize;
 use serde_json::{from_str, to_string};
 
-// Define your data structures here if needed (corresponding to deserialise/serialise in TypeScript)
-
-#[derive(Debug, Deserialize, Serialize)]
-struct Data {
-    // Define your fields here (matching the expected response from the API)
-}
-
-// NormalizeResponse function
-fn normalize_response(data: Option<&str>) -> Option<Data> {
+fn normalize_response<T: for<'de> Deserialize<'de>>(data: Option<&str>) -> Option<T> {
     if let Some(json_str) = data {
         let new_json_str = if let Ok(parsed_data) = from_str::<serde_json::Value>(json_str) {
             if parsed_data.get("included").is_none() {
@@ -28,26 +20,25 @@ fn normalize_response(data: Option<&str>) -> Option<Data> {
         } else {
             json_str.to_string()
         };
-        if let Ok(normalized_data) = from_str::<Data>(&new_json_str) {
+        if let Ok(normalized_data) = from_str::<T>(&new_json_str) {
             return Some(normalized_data);
         }
     }
     None
 }
 
-// NormalizeRequest function
-fn normalize_request(data: &Data) -> String {
+fn normalize_request<T: for<'de> Deserialize<'de>>(data: &T) -> String {
     // Implement your normalization logic here (corresponding to TypeScript's normalizeRequest)
     // For example:
-    // let serialized_data = to_string(&data).unwrap();
-    // let deserialized_data: serde_json::Value = from_str(&serialized_data).unwrap();
-    // let mut serialized_data_map = deserialized_data.as_object().unwrap().clone();
-    // serialized_data_map.remove("type");
-    // to_string(&serialized_data_map).unwrap()
+    let serialized_data = to_string(&data).unwrap();
+    let deserialized_data: serde_json::Value = from_str(&serialized_data).unwrap();
+    let mut serialized_data_map = deserialized_data.as_object().unwrap().clone();
+    serialized_data_map.remove("type");
+    to_string(&serialized_data_map).unwrap()
     // (Note: Replace `Data` with the appropriate data structure if needed)
 
     // Placeholder for this example
-    to_string(data).unwrap()
+    // to_string(data).unwrap()
 }
 
 struct RetryOptions {
@@ -69,13 +60,9 @@ impl APIClient {
         Self { api, retry_options }
     }
 
-    // Get function
-    async fn get<T: for<'de> Deserialize<'de>>(
-        &self,
-        url: &str,
-    ) -> Result<T, Box<dyn Error>> {
+    async fn get<T: for<'de> Deserialize<'de>>(&self, url: &str) -> Result<T, Box<dyn Error>> {
         let response = self.wrap_request(self.api.get(url)).await?;
-        if let Some(json_str) = response.text().await.unwrap().as_str() {
+        if let json_str = response.text().await.unwrap().as_str() {
             if let Some(data) = normalize_response(Some(json_str)) {
                 return Ok(data);
             }
@@ -83,7 +70,6 @@ impl APIClient {
         Err("Error deserializing data".into())
     }
 
-    // Post function
     async fn post<T: for<'de> Deserialize<'de>>(
         &self,
         url: &str,
@@ -93,7 +79,7 @@ impl APIClient {
         let response = self
             .wrap_request(self.api.post(url).json(&normalized_json))
             .await?;
-        if let Some(json_str) = response.text().await.unwrap().as_str() {
+        if let json_str = response.text().await.unwrap().as_str() {
             if let Some(data) = normalize_response(Some(json_str)) {
                 return Ok(data);
             }
@@ -101,17 +87,16 @@ impl APIClient {
         Err("Error deserializing data".into())
     }
 
-    // Put function
     async fn put<T: for<'de> Deserialize<'de>>(
         &self,
         url: &str,
-        json: &Data,
+        json: &T,
     ) -> Result<T, Box<dyn Error>> {
         let normalized_json = normalize_request(json);
         let response = self
             .wrap_request(self.api.put(url).json(&normalized_json))
             .await?;
-        if let Some(json_str) = response.text().await.unwrap().as_str() {
+        if let json_str = response.text().await.unwrap().as_str() {
             if let Some(data) = normalize_response(Some(json_str)) {
                 return Ok(data);
             }
@@ -119,11 +104,7 @@ impl APIClient {
         Err("Error deserializing data".into())
     }
 
-    // Delete function
-    async fn delete(
-        &self,
-        url: &str,
-    ) -> Result<(), Box<dyn Error>> {
+    async fn delete(&self, url: &str) -> Result<(), Box<dyn Error>> {
         self.wrap_request(self.api.delete(url)).await?;
         Ok(())
     }
